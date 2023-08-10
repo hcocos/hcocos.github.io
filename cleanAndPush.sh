e590d739d4b139dbe37c6ece95e7720daaa043a6
hcocos <henrycocos27@googlemail.com>
2023-08-11 01:29:24 +0200

    Neue Website hochgeladen

---
+++ cleanAndPush.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+
+
+echo "Eine Commit Nachricht eingeben:"
+read commitMessage
+
+
+echo -e "\nRepository von Auxillary Dateien befreit...\n"
+git add *
+git commit -m "$commitMessage"
+git push git@github.com:hcocos/hcocos.github.io.git
+echo -e "\nRepository has been pushed!...\n"
+

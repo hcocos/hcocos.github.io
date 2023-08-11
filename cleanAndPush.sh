fbc3f310ce0d8bf232c8703adb24ca15cabde222
hcocos <henrycocos27@googlemail.com>
2023-08-11 11:50:36 +0200

    Vorlesungen und About Seite angepasst

--- cleanAndPush.sh
+++ cleanAndPush.sh
@@ -1,7 +1,5 @@
 # /bin/bash
 
-
-
 echo "Eine Commit Nachricht eingeben:"
 read commitMessage
 
@@ -9,6 +7,6 @@ read commitMessage
 echo -e "\nRepository von Auxillary Dateien befreit...\n"
 git add *
 git commit -m "$commitMessage"
-git push git@github.com:hcocos/hcocos.github.io.git
+https://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@github.com/hcocos/hcocos.github.io.git
 echo -e "\nRepository has been pushed!...\n"
 
